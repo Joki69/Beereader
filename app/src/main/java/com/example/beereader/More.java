12c39822ab115335fdaec500eb4a65d532700140
Joki <jcarralero@elpuig.xeill.net>
2023-06-06 14:47:38 +0200

    Fragments100%

---
+++ app/src/main/java/com/example/beereader/More.java
@@ -0,0 +1,13 @@
+package com.example.beereader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class More extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browse);
+
+    }
+}
--- app/src/main/java/com/example/beereader/WelcomePage.java
+++ app/src/main/java/com/example/beereader/WelcomePage.java
@@ -2,19 +2,22 @@ package com.example.beereader;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WelcomePage#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class WelcomePage extends Fragment {
 
+public class WelcomePage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_welcome_page);
+
+    }
+    /*
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -28,14 +31,8 @@ public class WelcomePage extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WelcomePage.
-     */
+
+
     // TODO: Rename and change types and number of parameters
     public static WelcomePage newInstance(String param1, String param2) {
         WelcomePage fragment = new WelcomePage();
@@ -61,4 +58,5 @@ public class WelcomePage extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_welcome_page, container, false);
     }
+    */
 }
